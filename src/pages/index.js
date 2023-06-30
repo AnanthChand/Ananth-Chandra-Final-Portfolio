12354e9706fedfee1daf05a24e6b19178d55391c
Ananth Chandra <ananthinindia@gmail.com>
2023-06-30 13:31:59 +0530

    added resume.

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/Anantha MC (Ananth Chandra).pdf"
+                  href="/ananthchandra.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6
                   rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
